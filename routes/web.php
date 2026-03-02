4e2d898df480388eaa18235758a83c9f22b079e7
Mhd Rizky Ramadhan <m.rizkyramadhan2006@gmail.com>
2026-03-02 12:06:27 +0700

    menambahkan bagian news

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\NewsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/RuangController', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name(('dashboard'));
+
+Route::get('news',[NewsController::class,'index'])->name('news');
